5eda164434f2fb631f44ab39e140c1b4cf49a301
kennedyaraujo <kennedy.araujo@live.com>
2025-08-28 21:02:58 -0300

    feat: Projeto portado para o git com rotas implementadas

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/connect.vue'),
+      component: () => import('../views/Connect.vue'),
     },
 
      {
